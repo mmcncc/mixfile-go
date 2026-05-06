8239048f83e599e298c0930cebf597b7d28efd2e
mmcncc <mmcncc@gmail.com>
2026-05-06 17:35:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"mixfile-go/mixfile/server"
-
 	"net/http"
+
+	"mixfile/server"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	ser1 := &server.MixFileServer{
-		HttpClient:        &http.Client{},
-		DownloadTaskCount: 5,
-	}
-	fmt.Println("已启动服务器: 127.0.0.1:8080")
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", ser1))
+	ser1 := server.NewMixFileServer()
+
+	log.Println("server start :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", ser1))
 }
